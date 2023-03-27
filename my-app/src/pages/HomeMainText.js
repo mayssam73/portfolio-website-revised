19ae70879ec345449d967343168a19318ca1510b
mayssam73 <mayssamkalajo@gmail.com>
2023-03-27 12:34:17 -0500

    Fixed centering of main page text div

--- my-app/src/pages/HomeMainText.js
+++ my-app/src/pages/HomeMainText.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 export default function home_main_text() {
     return (
-        <div className="ml-8 text-xl text-center m-auto">
+        <div className="ml-8 text-xl text-center flex flex-col w-screen justify-center items-center">
             <div>
                 Nice to meet you!
             </div>
             <div className="mt-3 text-2xl tracking-[0.4em]">
-                I BRIDGE THE GAP BETWEEN <div className="brown inline-block">SOFTWARE DEVELOPMENT</div> AND <div className="brown inline-block">QA</div>
+                I BRIDGE THE GAP BETWEEN <br/><div className="brown inline-block">SOFTWARE DEVELOPMENT</div> AND <div className="brown inline-block">QA</div>
             </div>
             <div className="mt-3">
                 Cool, right?
