303c16f528f7bba7aa7b6afe8e35ac78ba5aeaa9
mayssam73 <mayssamkalajo@gmail.com>
2023-03-27 12:38:26 -0500

    Small UI changes

--- my-app/src/pages/SideBar.js
+++ my-app/src/pages/SideBar.js
@@ -9,10 +9,10 @@ export default function side_bar() {
             <div className="flex flex-row">
                 {/* Fix the responsiveness of icons */}
                 <Link href="https://github.com/mayssam73">
-                    <FontAwesomeIcon icon={["fab", "github"]} rotation={270} size="2xl" className="ml-4 mt-[64vh]"/>
+                    <FontAwesomeIcon icon={["fab", "github"]} rotation={270} size="2xl" className="ml-4 mt-[57vh]"/>
                 </Link>
                 <Link href="https://www.linkedin.com/in/mayssam-kalajo/">
-                    <FontAwesomeIcon icon={["fab", "linkedin"]} rotation={270} size="2xl" className="-ml-7 mt-[69vh]"/>
+                    <FontAwesomeIcon icon={["fab", "linkedin"]} rotation={270} size="2xl" className="-ml-7 mt-[63vh]"/>
                 </Link>
             </div>
         </div>
